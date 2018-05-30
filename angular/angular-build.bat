b6cab5e0b089ecb2eca6f14f205474d40917bd7d
Alan Flaherty <alanflaherty@mail.com>
2018-05-30 04:05:11 +0100

    Added initial angular build from csproj file, this looks for an existing dist folder and just renames it with the current date and time, possibly should use folder date-time created.

---
+++ angular/angular-build.bat
@@ -0,0 +1,26 @@
+﻿REM @echo off
+
+REM setlocal and !variable! instead of %variable% to 
+REM prevent variable expansion from start
+setlocal ENABLEDELAYEDEXPANSION
+
+REM Move 'dist' into dist-DD-MM-YYYY-HH-MM
+echo Checking if .\dist exists
+
+if exist .\dist (
+    echo dist folder exists renaming.. 
+
+    set _timepart=%time: =0%
+    set _timepart=!_timepart:~0,5!
+    set _timepart=!_timepart: =0!
+    set _timepart=!_timepart::=-!
+
+    set _datepart=%date:/=-%
+    set _newfilename=dist-!_datepart!-!_timepart!
+
+    echo Moving dist to !_newfilename!
+    move dist !_newfilename!
+) else (
+    echo "No Existing dist folder
+)
+
